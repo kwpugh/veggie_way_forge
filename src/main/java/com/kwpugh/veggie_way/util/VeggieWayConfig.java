85714c89f6b4c3286dad1a435231bbca53c5c18f
BuildTools <unconfigured@null.spigotmc.org>
2020-01-31 21:18:56 -0600

    updates to config file

--- src/main/java/com/kwpugh/veggie_way/util/VeggieWayConfig.java
+++ src/main/java/com/kwpugh/veggie_way/util/VeggieWayConfig.java
@@ -38,71 +38,101 @@ public class VeggieWayConfig
 	
 	public static void init(ForgeConfigSpec.Builder server)
 	{	
-		server_output = server.comment("Normal Food Hunger and Saturation Values");
+		server_output = server.comment("Normal Food Hunger and Saturation Values").push("Normal Foods");
 		
-		apple_pie_hunger = server
-				.comment("Hunger value:")
-				.defineInRange("apple_pie_hunger", 7, 0, 30);
-		apple_pie_saturation = server
-				.comment("Saturation value:")
-				.defineInRange("apple_pie_saturation", 1, 0, 10);
-
-		melon_pie_hunger = server
-				.comment("Hunger value:")
-				.defineInRange("melon_pie_hunger", 7, 0, 30);
-		melon_pie_saturation = server
-				.comment("Saturation value:")
-				.defineInRange("melon_pie_saturation", 1, 0, 10);
+			server_output = server.comment("Normal Foods").push("Apple");
+			apple_pie_hunger = server
+					.comment("Hunger value:")
+					.defineInRange("apple_pie_hunger", 7, 0, 30);
+			apple_pie_saturation = server
+					.comment("Saturation value:")
+					.defineInRange("apple_pie_saturation", 2, 0, 10);
+			server_output.pop();
 		
-		berry_pie_hunger = server
-				.comment("Hunger value:")
-				.defineInRange("berry_pie_hunger", 7, 0, 30);
-		berry_pie_saturation = server
-				.comment("Saturation value:")
-				.defineInRange("berry_pie_saturation", 1, 0, 10);		
-
-		pumpkin_soup_hunger = server
-				.comment("Hunger value:")
-				.defineInRange("pumpkin_soup_hunger", 7, 0, 30);
-		pumpkin_soup_saturation = server
-				.comment("Saturation value:")
-				.defineInRange("pumpkin_soup_saturation", 1, 0, 10);
-
-		server_output = server.comment("Super Food Hunger and Saturation Values");
 		
-		energy_drink_hunger = server
-				.comment("Hunger value:")
-				.defineInRange("energy_drink_hunger", 9, 0, 30);
-		energy_drink_saturation = server
-				.comment("Saturation value:")
-				.defineInRange("energy_drink_saturation", 1, 0, 10);
-
-		shake_hunger = server
-				.comment("Hunger value:")
-				.defineInRange("shake_hunger", 10, 0, 30);
-		shake_saturation = server
-				.comment("Saturation value:")
-				.defineInRange("shake_saturation", 1, 0, 10);
-
-		smoothie_hunger = server
-				.comment("Hunger value:")
-				.defineInRange("smoothie_hunger", 10, 0, 30);
-		smoothie_saturation = server
-				.comment("Saturation value:")
-				.defineInRange("smoothie_saturation", 1, 0, 10);
+			server_output = server.comment("Normal Foods").push("Melon");
+			melon_pie_hunger = server
+					.comment("Hunger value:")
+					.defineInRange("melon_pie_hunger", 7, 0, 30);
+			melon_pie_saturation = server
+					.comment("Saturation value:")
+					.defineInRange("melon_pie_saturation", 2, 0, 10);
+			server_output.pop();
+			
 		
-		energyBar_hunger = server
-				.comment("Hunger value:")
-				.defineInRange("energyBar_hunger", 13, 0, 30);
-		energyBar_saturation = server
-				.comment("Saturation value:")
-				.defineInRange("energyBar_saturation", 3, 0, 10);		
+			server_output = server.comment("Normal Foods").push("Berry");
+			berry_pie_hunger = server
+					.comment("Hunger value:")
+					.defineInRange("berry_pie_hunger", 7, 0, 30);
+			berry_pie_saturation = server
+					.comment("Saturation value:")
+					.defineInRange("berry_pie_saturation", 2, 0, 10);		
+			server_output.pop();
+		
+		
+			server_output = server.comment("Normal Foods").push("Pumpkin");
+			pumpkin_soup_hunger = server
+					.comment("Hunger value:")
+					.defineInRange("pumpkin_soup_hunger", 7, 0, 30);
+			pumpkin_soup_saturation = server
+					.comment("Saturation value:")
+					.defineInRange("pumpkin_soup_saturation", 2, 0, 10);
+			server_output.pop();
+			
+		server_output.pop();
+		
+		
 
-		superFoodBar_hunger = server
-				.comment("Hunger value:")
-				.defineInRange("superFoodBar_hunger", 13, 0, 30);
-		superFoodBar_saturation = server
-				.comment("Saturation value:")
-				.defineInRange("superFoodBar_saturation", 3, 0, 10);
+		server_output = server.comment("Super Food Hunger and Saturation Values").push("Super Foods");
+		
+			server_output = server.comment("Super Foods").push("Energy");
+			energy_drink_hunger = server
+					.comment("Hunger value:")
+					.defineInRange("energy_drink_hunger", 9, 0, 30);
+			energy_drink_saturation = server
+					.comment("Saturation value:")
+					.defineInRange("energy_drink_saturation", 2, 0, 10);
+			server_output.pop();
+		
+		
+			server_output = server.comment("Super Foods").push("Shake");
+			shake_hunger = server
+					.comment("Hunger value:")
+					.defineInRange("shake_hunger", 10, 0, 30);
+			shake_saturation = server
+					.comment("Saturation value:")
+					.defineInRange("shake_saturation", 3, 0, 10);
+			server_output.pop();
+		
+		
+			server_output = server.comment("Super Foods").push("Smoothie");
+			smoothie_hunger = server
+					.comment("Hunger value:")
+					.defineInRange("smoothie_hunger", 10, 0, 30);
+			smoothie_saturation = server
+					.comment("Saturation value:")
+					.defineInRange("smoothie_saturation", 3, 0, 10);
+			server_output.pop();
+		
+		
+			server_output = server.comment("Super Foods").push("Bar");
+			energyBar_hunger = server
+					.comment("Hunger value:")
+					.defineInRange("energyBar_hunger", 13, 0, 30);
+			energyBar_saturation = server
+					.comment("Saturation value:")
+					.defineInRange("energyBar_saturation", 4, 0, 10);	
+			
+			superFoodBar_hunger = server
+					.comment("Hunger value:")
+					.defineInRange("superFoodBar_hunger", 13, 0, 30);
+			superFoodBar_saturation = server
+					.comment("Saturation value:")
+					.defineInRange("superFoodBar_saturation", 4, 0, 10);
+			server_output.pop();
+		
+		
+		server_output.pop();
+		
 	}
 }
